1f639de9946899e6d6ff36eee50eee0e96111043
BensonFeng <bensonfeng@outlook.com>
2022-12-31 21:19:00 -0500

    components basic layout

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Header, Nav, Main, Footer } from "./components";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+import Nav from "./Nav";
+
+export { Footer, Header, Main, Nav };
